0b09f40a480545d5585b177723bc11b14b4fb3a1
Asheley Brown <ABrown119@humana.com>
2020-07-29 18:20:53 -0400

    Post Links work

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -11,32 +11,41 @@ import blogPost from '../../data/blog.json';
 const BlogPost = (props) => {
 
     const [post, setPost] = useState({
-        blogTitle: " "
+        id: " " ,
+        blogCategory: " " ,
+        blogTitle : " " ,
+        blogImage: " " ,
+        blogText: " "
     });
+
+    const [postId, setPostId] = useState('')
 
     useEffect(() => {
         const postId = props.match.params.postId;
-        const post = blogPost.data.find(post => post.id == postId )
-        setPost(post)
-    }, post);
+        const post = blogPost.data.find(post => post.id == postId );
+        setPost(post);
+        setPostId(postId)
+    }, [post, props.match.params.postId]);
+
+    if(post.blogImage == " ") return null
 
   return(
         <div className="blogPostContainer"> 
             <Card>
                 <div className="blogHeader">
-                    <span className="blogCategory">Featured</span>
+                    <span className="blogCategory">{post.blogCategory}</span>
                     <h1 className="postTitle">{post.blogTitle}</h1>
                     <p>Come have a picnic at Cherokee Park</p>
                     <span className="postedBy">Created by Asheley Howard Brown</span>
                 </div>
 
                 <div className="postImgContainer">                    
-                    <img src={require('../../blogPostImages/cherokeepark.jpg')} alt="Cherokee Park Image"/>
+                    <img src={require(`../../blogPostImages/${post.blogImage}`)} alt="Cherokee Park Image"/>
                 </div>
               
               <div className="postList">
-                  <h3>Post Title</h3>
-                  <p>Asheley Howard Brown</p>
+                  <h3>{post.blogTitle}</h3>
+                  <p>{post.blogText}</p>
                 </div>
             </Card>
         </div>
