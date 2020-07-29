135bc0f41fd34978cc4af119c06a3e7ea9f77217
Asheley Brown <ABrown119@humana.com>
2020-07-29 11:31:43 -0400

    front end

---
+++ src/components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Logo
+**/
+
+const Logo = (props) => {
+  return(
+    <div className="logo">
+    <a href="#">Stay Safe & Play Louisville</a>    
+    </div>
+   )
+
+ }
+
+export default Logo
