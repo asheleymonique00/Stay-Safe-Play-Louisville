338827fbbf856838dd3ba85f7052664785c08abe
Asheley Brown <ABrown119@humana.com>
2020-07-29 22:58:30 -0400

    Home Page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,16 @@
-import React from 'react'
+// import React, { Component } from 'react';
+// import { Route, Link, withRouter } from 'react-router-dom';
+// import Modal from 'react-modal';
+
+
+// //Custom Components 
+// import SignUp from './Components/SignUp';
+// import Login from './Components/Login';
+
+// //Import Axios Calls
+// import { signUpUser, loginUser} from './Service/api_helper';
+
+import React from 'react';
 import './style.css';
 
 /**
@@ -7,12 +19,24 @@ import './style.css';
 **/
 
 const Header = (props) => {
+// class Header extends Component {
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             currentUser: null,
+//             modal: false
+//         }
+//     }
+
+
+// render() {
   return(
         <header className="header">
             <nav className="headerMenu">
-                <a href="#">Home</a>
+                <a href="#">Sign Up</a>
+                <a href="#">Login</a>
                 <a href="#">About Us</a>
-                <a href="#">Contact Us</a>
             </nav>
             <div>
                 social media links
@@ -22,4 +46,6 @@ const Header = (props) => {
 
  }
 
-export default Header
+export default Header;
+
+// export default withRouter(Header);
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import './style.css';
+import Card from '../../components/UI/Card'
+
 
 
 const Home = props => {
+
+    const listHeight= 1000;
+    const listStyle = {
+      height: listHeight+'px',
+      overflow: 'hidden'
+    }
+
+    // const sidePictures= listHeight/3;
+
     return (
-        <div>
-          Home
-        </div>
+      <div>
+        <Card>
+          <div className="listPost" style={listStyle}>
+    <section /*style={{ width: '70%'}}*/>
+            <div className="homePage">
+            <img src={"https://s3.amazonaws.com/tw-inlineimages/99238/0/0/4c5929880f9486b543914771890b580e.jpg"} alt="City of Louisville Image" />
+            </div>
+          </section>
+          {/* <section className={'allSidePictures'} style={{ width: '30%'}}>
+          <div style={{ height: `${sidePictures}px`}}>
+            <img src={"https://s3.amazonaws.com/tw-inlineimages/99238/0/0/4c5929880f9486b543914771890b580e.jpg"} alt="City of Louisville Image" />
+            </div>
+            <div style={{ height: `${sidePictures}px`}}>
+            <img src={"https://s3.amazonaws.com/tw-inlineimages/99238/0/0/4c5929880f9486b543914771890b580e.jpg"} alt="City of Louisville Image" />
+            </div>
+            <div style={{ height: `${sidePictures}px`}}>
+            <img src={"https://s3.amazonaws.com/tw-inlineimages/99238/0/0/4c5929880f9486b543914771890b580e.jpg"} alt="City of Louisville Image" />
+            </div >
+          </section> */}
+          </div>
+        </Card>
+     
+     </div>
     );
 }
 
